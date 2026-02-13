4394243b97f26ec8fe35696b33340fe20422fdcc
Richard <Richardtshi23@Gmail.com>
2026-02-14 00:28:38 +0200

    env path fix

--- src/environments/environment-prod.ts
+++ src/environments/environment-prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://holy-essence-backend-2.onrender.com/api', // <-- PASTE YOUR LIVE RENDER API URL HERE
-  apiImageUrl: 'https://holy-essence-backend-2.onrender.com/api'
+  apiImageUrl: 'https://holy-essence-backend-2.onrender.com'
 };
